fd2b0a4c445b977f0a4951572a182c8dea9a6596
Tom Havlik <keres993@gmail.com>
2020-10-30 15:45:07 -0500

    ...

--- src/dataset/doom_stills.py
+++ src/dataset/doom_stills.py
@@ -17,8 +17,25 @@ class DoomStillsDataset(data.Dataset):
     def __len__(self):
         raise NotImplementedError
 
+
 if __name__ == '__main__':
     import youtube_dl
+    import cv2
+    
+    def process_video(video):
+        cap = cv2.VideoCapture(f'{video["id"]}.{video["ext"]}')
+        # Check if camera opened successfully
+        if not cap.isOpened(): 
+            raise ValueError(f"Error opening video stream or file")
+        while(cap.isOpened()):
+            ret, frame = cap.read()
+            if ret == True:
+                # frame is good
+                pass
+            else:
+                break
+        cap.release()
+    
     items = [
         "https://www.youtube.com/watch?v=4vkFdcbUtBU",
         "https://www.youtube.com/watch?v=apo9Vb-5pWo&list=PLm8AwdYOntbLy7vbWt42N555iuIN27XW0",
@@ -34,9 +51,9 @@ if __name__ == '__main__':
             if 'entries' in result:
                 # It is a playlist
                 for video in result['entries']:
-                    print(video)
+                    process_video(video)
             else:
                 # Just a single video
                 video = result
-                print(video)
-            #f'{video.id}.{video.ext}'
+                process_video(video)
+            

a797658628b32105ba0835f6ac83bb644ae02823
Tom Havlik <keres993@gmail.com>
2020-10-30 14:54:49 -0500

    ...

--- src/dataset/doom_stills.py
+++ src/dataset/doom_stills.py
@@ -16,3 +16,19 @@ class DoomStillsDataset(data.Dataset):
 
     def __len__(self):
         raise NotImplementedError
+
+if __name__ == '__main__':
+    import youtube_dl
+    ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s.%(ext)s'})
+    with ydl:
+        result = ydl.extract_info(
+            'http://www.youtube.com/watch?v=BaW_jenozKc',
+            download=False # We just want to extract the info
+        )
+    if 'entries' in result:
+        # Can be a playlist or a list of videos
+        video = result['entries'][0]
+    else:
+        # Just a video
+        video = result
+    print(video)
